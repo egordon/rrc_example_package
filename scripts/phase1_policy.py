42b30f0b330cc9400ed69902a28ba8a88ed64fb8
Rishabh Madan <rishabhmadan96@gmail.com>
2020-10-28 18:59:07 +0530

    try difficulty 3

--- scripts/phase1_policy.py
+++ scripts/phase1_policy.py
@@ -638,7 +638,7 @@ episode_length = 2 * 60 * 1000
 def main():
     # the difficulty level and the goal pose (as JSON string) are passed as
     # arguments
-    difficulty = 2
+    difficulty = 3
     goal_pose_json = sys.argv[2]
     # goal = json.loads(goal_pose_json)
     goal_pose = move_cube.sample_goal(difficulty)

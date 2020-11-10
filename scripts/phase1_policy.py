c7f0f90a1c6f665e93e97ade4f77914416ae3abd
Rishabh Madan <rishabhmadan96@gmail.com>
2020-11-10 23:56:27 +0530

    change

--- scripts/phase1_policy.py
+++ scripts/phase1_policy.py
@@ -174,7 +174,7 @@ class StateSpacePolicy:
         desired = np.tile(curr_cube_position, 3) + \
             (self.CUBE_SIZE + 0.015) * np.hstack(locs)
 
-        desired[self.manip_arm * 3:] -= 0.015 * np.hstack(locs)[self.manip_arm * 3:]
+        desired[self.manip_arm * 3: (self.manip_arm + 1)*3] -= 0.015 * np.hstack(locs)[self.manip_arm * 3: (self.manip_arm + 1)*3]
 
         err = desired - current
         if np.linalg.norm(err) < 0.01:

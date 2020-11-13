f7f3ce67d8f1d258a9bdd0ca57b3020d8f1a5f06
Rishabh Madan <rishabhmadan96@gmail.com>
2020-11-13 18:54:21 +0530

    test l1 again

--- scripts/phase1_policy.py
+++ scripts/phase1_policy.py
@@ -768,7 +768,7 @@ def main():
     # goal = json.loads(goal_pose_json)
 
     # TODO: Comment before submission
-    difficulty = 3
+    difficulty = 1
     goal_pose = move_cube.sample_goal(difficulty)
     goal = {'position': goal_pose.position,
             'orientation': goal_pose.orientation}

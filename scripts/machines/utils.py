7473b0ad2d3b6fb0d5cc468bcad57acae529fa14
Rishabh Madan <rishabhmadan96@gmail.com>
2020-12-11 02:23:22 +0530

    utils change

--- scripts/machines/utils.py
+++ scripts/machines/utils.py
@@ -27,7 +27,7 @@ def get_rest_arm2(observation):
     manip_axis = R.from_quat(current).apply(axis)
 
     rot = R.from_quat(current)
-    yaw = np.arctan2(manip_axis[1], manip_axis[0]) * 180. / np.pi
+    yaw = np.rad2deg(np.arctan2(manip_axis[1], manip_axis[0]))
 
     # if yaw < 60 and yaw > -60:
     #     rest_arm = 0
@@ -37,9 +37,9 @@ def get_rest_arm2(observation):
     #     rest_arm = 2
 
     if yaw > -90.0 and yaw <= 30.0:
-        rest_arm = 0
-    elif yaw > 30.0 and yaw <= 150:
         rest_arm = 1
+    elif yaw > 30.0 and yaw <= 150:
+        rest_arm = 0
     else:
         rest_arm = 2
     manip_axis[2] = 0.
